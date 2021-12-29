89721202e4b99f4a38208988a9dd314a765d5a76
hyun <coderhyun476@gmail.com>
2021-12-29 23:59:58 +0900

    Implement index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleProp, TextStyle, ViewStyle, LayoutChangeEvent, NativeSyntheticEvent, TextLayoutEventData } from 'react-native';
+
+export interface ReadMoreProps {
+    style?: StyleProp<TextStyle>;
+    seeMoreStyle?: StyleProp<TextStyle>;
+    seeLessStyle?: StyleProp<TextStyle>;
+    wrapperStyle?: StyleProp<ViewStyle>;
+    children?: React.ReactNode;
+    numberOfLines?: number;
+    seeMoreText?: string;
+    seeLessText?: string;
+    animate?: boolean;
+    customTextComponent?: React.ReactNode;
+    ellipsis?: string;
+    allowFontScaling?: boolean;
+    onExpand?: () => void;
+    onCollapse?: () => void;
+    expandOnly?: boolean;
+    seeMoreOverlapCount?: number;
+    debounceSeeMoreCalc?: number;
+    onLayout?: (event: LayoutChangeEvent) => void;
+    onTextLayout?: (event: NativeSyntheticEvent<TextLayoutEventData>) => void;
+    onReady?: () => void;
+}
+declare const ReadMore: React.FC<ReadMoreProps>;
+export default ReadMore;
